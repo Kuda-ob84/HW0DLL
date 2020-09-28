e456316d068b789f6aaeab23332d7a8e0f30acc9
Kuda-ob84 <65267697+Kuda-ob84@users.noreply.github.com>
2020-09-28 09:40:49 +0600

    Добавьте файлы проекта.

---
+++ HW0DLL/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Line;
+using Square;
+
+namespace HW0DLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Площадь треугольника: {Equation.TriangleArea(5, 8)}");
+            Console.WriteLine($"Площадь квадрата: {Equation.SquareArea(15)}");
+            Console.WriteLine($"Площадь прямоугольника: {Equation.RecktangleArea(6, 4)}");
+
+            string a = "Меня зовут даукен ";
+            string palindrom = "Довод";
+            Console.WriteLine($"Переворот: {LineEquation.Reverse(a)}");
+            Console.WriteLine($"Строка {palindrom}: {LineEquation.IsPalindrom(palindrom)}");
+            Console.WriteLine($"Количество предложений в \"{a}\": {LineEquation.HowManyWords(a)}");
+            Console.ReadKey();
+
+        }
+    }
+}
